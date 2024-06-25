086869baf9186f44364e3d34313bd787b070b669
TsunaMaru0103 <Tomoya_Suzuki@jmas.co.jp>
2024-06-25 10:44:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDoApplication
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/TsunaMaru0103/ToDoApplication)
---
+++ main.js
@@ -0,0 +1,24 @@
+/**
+ * const,letの変数宣言
+ */
+// var val1 = 'var変数';
+// console.log(val1);
+
+// val1 = 'var変数を上書き';
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = '再宣言したよ';
+// console.log(val1);
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// val2 = '上書きしました。';
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = '再宣言を試みています';
+
+const val3 = 'const変数です';
+console.log(val3);
